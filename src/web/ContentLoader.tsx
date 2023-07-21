f11348f4549e5f8c93420d5ea4859814c3105630
manik.ravinder.pal <manik.ravinder.pal@accenture.com>
2023-07-21 19:07:07 +0530

    Initial commit

---
+++ src/web/ContentLoader.tsx
@@ -0,0 +1,9 @@
+
+
+import { Facebook, IContentLoaderProps } from '.'
+import Svg from './Svg'
+
+const ContentLoader: React.FC<IContentLoaderProps> = props =>
+  props.children ? <Svg {...props} /> : <Facebook {...props} />
+
+export default ContentLoader
